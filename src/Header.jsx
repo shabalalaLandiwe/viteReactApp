2a40c6ba4020226c9d4e2a3a90e7a6023977dae5
FemmeCode <shabalala.landiwe@gmail.com>
2025-08-20 15:02:32 +0200

    installed tailwindcss dependancies

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,17 @@
 
 
 function Header(){
+    // return() in React doesn’t return just one component — 
+    // it returns one JSX expression,
     return(
         <header> 
             <h1>Artisans</h1>
 
             <nav>
                 <ul>
-                    <li ><a href="">Home</a></li>
-                    <li ><a href="">About</a></li>
-                    <li ><a href="">Sign up</a></li>
+                    <li className="home links"><a href="">Home</a></li>
+                    <li className="home links"><a href="">About</a></li>
+                    <li className="home links" ><a href="">Sign up</a></li>
                     
                     <li></li>
                 </ul>
