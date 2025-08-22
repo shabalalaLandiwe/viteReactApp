e5bc2182f398e9c0006201529125a1f5f5ffc46d
FemmeCode <shabalala.landiwe@gmail.com>
2025-08-23 00:09:14 +0200

    added some tailwind styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
-import Login from './Login.jsx';
-import Home from "./Home.jsx";
+// import Login from './Login.jsx';
+// import Home from "./Home.jsx";
 
 function Header(){
     // return() in React doesn’t return just one component — 
     // it returns one JSX expression,
     return(
-        <header className="Header"> 
-            <h1>Artisans</h1>
-    
-            <nav>
-                {/* Link: Creates navigation links that update the URL */}
-                 
-                <Link to ="/Login">Sign-up</Link> 
-            </nav>
+        <header class="bg-amber-700 opacity-60 flex-2 p-5"> 
+            <div class="row-start-1 min-h-18 inline-table"  >
+                <h1 class="text-blue-950 backdrop-opacity-100 p-2">Artist hands</h1>
+                <h4 class="pl-3"> A community of Artisans </h4>
+            </div>
 
+{/*  'flex and inline-table have apply the same propeties but flex handles 
+them as separate containers */}
+            <div class ="flex ma flex-row-reverse  space-x-4 space-x-reverse">
+                
+                <div class="inline-grid mt-" >
+                        {/* Link: Creates navigation links that update the URL */}
+                    <Link to ="/">Home</Link>  {" "}
+                </div>
+                <div class="inline-grid">
+                        {/* <Link to ="/about">About</Link>  */}
+                    <Link to ="/Login">Sign-up</Link> 
+                </div>
+                   
+            </div>
         </header>
     );
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@
 function Home(){
     return(
         
-        <div className="bg-purple-600 text-white p-4  shadow-md bg-cover bg-center max-h [220px">
+        <div className="bg-amber-200 backdrop-opacity-5 text-white p-4  shadow-md md:w-screen h-150 p-2" >
             <h1> HOOOOOOOMMMMMEEEE</h1>
         </div>
         
