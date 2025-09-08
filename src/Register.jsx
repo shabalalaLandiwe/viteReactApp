397a8234eb213ad007fb4885a1be57f1f727560e
FemmeCode <shabalala.landiwe@gmail.com>
2025-09-08 20:17:54 +0200

    added handleChange constant and defined input types for form data

--- src/Register.jsx
+++ src/Register.jsx
@@ -17,35 +17,64 @@ function Register(){
         AddressLine3:'',
         PostalCode: '',
         email: '',
+        password:'',
 
     })
+
+
+// This function will dynamically update the corresponding field in the formData object
+//  based on the name attribute of the input that triggered the change.
+    const HandleChange = (e) => {
+
+        // Optional: prevents page reload if used in a form
+        e.preventDefault();  
+        const{name, value} = e.target;
+
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]:value,
+        }));
+    };
 
     return(
         <form>
             <label>Name:
-                <input></input>
+                <input type="text" name="name" 
+                value={formData.Name}  onChange={HandleChange} />
             </label>
-            <label>Surname:
-                <input></input>
+            <label >Surname:
+                <input type="text" name="surnamename" 
+                value={formData.Surname}  onChange={HandleChange}/>
             </label>
             <label>ID:
-                <input></input>
+                <input type="text" name="name" 
+                value={formData.ID}  onChange={HandleChange}/>
             </label>
             <label>Birthday:
-                <input type="date"></input>
+                <input type="text" name="birthday" 
+                value={formData.Birthday}  onChange={HandleChange} />
             </label>
             <label> Address Line 1
-                <input></input>
+                <input type="text" name="addressline1" 
+                value={formData.AddressLine1}  onChange={HandleChange}/>
             </label>
             <label> Address Line 2
-                <input></input>
+                <input type="text" name="addressline2" 
+                value={formData.AddressLine2}  onChange={HandleChange}/>
             </label>
             <label> Address Line 
                 
-                <input></input>
+                <input type="text" name="addressline3" 
+                value={formData.AddressLine3}  onChange={HandleChange}/>
             </label>
-            
-            
+            <label > Email:
+                <input type="text" name="email" 
+                value={formData.email}  onChange={HandleChange}/>
+            </label>
+            <label >Create password:
+                <input type="int" name="password" 
+                value={formData.password}  onChange={HandleChange}/>
+            </label>
 
         </form>
     );
